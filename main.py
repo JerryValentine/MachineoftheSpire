dd1342d68e80caca9770a9c1b06779bcaf0b4925
Jerry Valentine <JerryCValentine@gmail.com>
2024-02-20 01:59:03 -0500

    Added card testing and cleaned up

--- main.py
+++ main.py
@@ -1,36 +1,44 @@
-from units.enemies.standard import jaw_worm
+from units.player import Player
+from units.enemies.standard.jaw_worm import JawWorm
+from cards.ironclad.strike import Strike
 
-enemy = jaw_worm.JawWorm()
-enemy2 = jaw_worm.JawWorm()
-# print(enemy.hp)
-# print(enemy2.hp)
-# enemy.act(2)
-# print(enemy2.hp)
+# Object Instantiation
 
-round_1 = enemy.get_action(round=2)
-round_2 = enemy.get_action(round=3)
-round_3 = enemy.get_action(round=4)
-round_4 = enemy.get_action(round=5)
-round_5 = enemy.get_action(round=6)
-round_6 = enemy.get_action(round=7)
-round_7 = enemy.get_action(round=8)
+player = Player(100)
+strike = Strike()
+enemy = JawWorm()
+enemy2 = JawWorm()
 
-print(round_1())
-print(round_2())
-print(round_3())
-print(round_4())
-print(round_5())
-print(round_6())
-print(round_7())
 
-print('end turn')
+# Enemy Testing
 
-print(round_1(is_acting=True))
-print(round_2(is_acting=True))
-print(round_3(is_acting=True))
-print(round_4(is_acting=True))
-print(round_5(is_acting=True))
-print(round_6(is_acting=True))
-print(round_7(is_acting=True))
-print(enemy.sheild)
-print(enemy.strength)
+# round_1 = enemy.get_action(round=2)
+# round_2 = enemy.get_action(round=3)
+# round_3 = enemy.get_action(round=4)
+# round_4 = enemy.get_action(round=5)
+
+# print(round_1())
+# print(round_2())
+# print(round_3())
+# print(round_4())
+# print(round_5())
+# print(round_6())
+# print(round_7())
+
+# print('end turn')
+
+# print(round_1(is_acting=True))
+# print(round_2(is_acting=True))
+# print(round_3(is_acting=True))
+# print(round_4(is_acting=True))
+# print(enemy.shield)
+# print(enemy.strength)
+
+
+# Card testing
+
+print(enemy)
+strike.play(player, enemy)
+strike.play(player, enemy)
+strike.play(player, enemy)
+print(enemy)
