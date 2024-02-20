e14566412c2b0f1133c22f524a9078921f530a5c
Jerry Valentine <JerryCValentine@gmail.com>
2024-02-19 19:37:23 -0500

    first stab at cards

---
+++ cards/card.py
@@ -0,0 +1,9 @@
+class Card():
+    def __init__(self):
+        self.cost = 0
+        self.dmg = 0
+        self.block = 0
+
+    def play(self, player, target):
+        print("card not implemented yet")
+        pass
---
+++ cards/ironclad/strike.py
@@ -0,0 +1,15 @@
+from ..card import Card
+from ...units.player import Player
+from ...units.enemies.enemy import Enemy
+
+
+class Strike(Card):
+    def __init__(self):
+        super().__init__()
+        self.cost = 1
+        self.attack = 6
+
+    def play(self, player: Player, target: Enemy):
+        dmg = self.attack + player.strength
+        if (player.weakened > 0):
+            dmg = dmg * 0.75
