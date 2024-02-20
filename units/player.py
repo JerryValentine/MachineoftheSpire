68a98a4f01d42be77b97e7ca407afd2f9e7a5f05
Jerry Valentine <JerryCValentine@gmail.com>
2024-02-20 18:42:20 -0500

    Added deck, hand, discard logic along with print functions

--- units/player.py
+++ units/player.py
@@ -1,9 +1,16 @@
+import random
+from copy import deepcopy as copy
 from units.unit import Unit
 
 
 class Player(Unit):
     def __init__(self, hp):
         super().__init__(hp)
+        self.deck = []
+        self.hand = []
+        self.discard = []
+        self.exhaust = []
+        self.draw = 5
         self.confused = False
         self.dexterity = 0
         self.dexterity_down = 0
@@ -15,3 +22,35 @@ class Player(Unit):
         self.entangled = False
         self.hex = False
         self.no_block = 0
+
+    def _generate_starting_deck(self):
+        print("deck not implemented yet")
+        pass
+
+    def draw_hand(self):
+        if len(self.deck) < self.draw:
+            self.hand = self.deck[:self.draw-len(self.deck)]
+            self.deck = copy(self.discard)
+            self.discard = []
+            random.shuffle(self.deck)
+            self.hand += self.deck[:self.draw-len(self.hand)]
+        else:
+            self.hand = self.deck[:self.draw]
+            self.deck = self.deck[self.draw:]
+
+    def play_card(self, card, target):
+        card.play(self, target)
+        self.hand.remove(card)
+        self.discard.append(card)
+
+    def print_deck(self):
+        for i, card in enumerate(self.deck):
+            print(f"{i}. {card.__class__.__name__}")
+
+    def print_hand(self):
+        for i, card in enumerate(self.hand):
+            print(f"{i}. {card.__class__.__name__}")
+
+    def print_discard(self):
+        for i, card in enumerate(self.discard):
+            print(f"{i}. {card.__class__.__name__}")
