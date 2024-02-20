58ad1bcc7ac19fcbccbf0814a338873b53ed7d82
Jerry Valentine <JerryCValentine@gmail.com>
2024-02-20 02:00:08 -0500

    Fixed variable names and abstracted calculating dmg

--- cards/ironclad/strike.py
+++ cards/ironclad/strike.py
@@ -1,15 +1,14 @@
-from ..card import Card
-from ...units.player import Player
-from ...units.enemies.enemy import Enemy
+from cards.card import Card
+from units.player import Player
+from units.enemies.enemy import Enemy
 
 
-class strike(Card):
+class Strike(Card):
     def __init__(self):
         super().__init__()
         self.cost = 1
-        self.attack = 6
+        self.dmg = 6
 
     def play(self, player: Player, target: Enemy):
-        dmg = self.attack + player.strength
-        if (player.weakened > 0):
-            dmg = dmg * 0.75
+        dmg = self.calculate_dmg(player, target)
+        target.take_dmg(dmg)
