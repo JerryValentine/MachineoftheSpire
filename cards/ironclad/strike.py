a7bf38f93765b0b3dc18536698feec097a52cbee
Jerry Valentine <JerryCValentine@gmail.com>
2024-02-20 18:41:50 -0500

    Added card type and upgrade function

--- cards/ironclad/strike.py
+++ cards/ironclad/strike.py
@@ -1,4 +1,4 @@
-from cards.card import Card
+from cards.card import Card, CardType
 from units.player import Player
 from units.enemies.enemy import Enemy
 
@@ -6,8 +6,12 @@ from units.enemies.enemy import Enemy
 class Strike(Card):
     def __init__(self):
         super().__init__()
+        self.type = CardType.ATTACK
         self.cost = 1
         self.dmg = 6
+
+    def upgrade(self):
+        self.dmg = 9
 
     def play(self, player: Player, target: Enemy):
         dmg = self.calculate_dmg(player, target)
